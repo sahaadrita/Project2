1a1433224f67c6b7bdc2909d488a4e24ced906f1
sahaadrita <adrita.saha@optimizeitsystems.com>
2021-12-21 18:40:31 +0530

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -10,13 +10,30 @@ const Login = (props) => {
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+  useEffect(()=>{
+    console.log('EFFECT RUNNING')
+    return ()=>{
+      console.log('EFFECT CLEANUP')
+    }
+  },[])
+  useEffect(()=>{
+    const identifier = setTimeout(()=>{
+    console.log('check form validity')
+    
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+   return () => {
+     console.log ('CLEANUP')
+     clearTimeout(identifier)
+   }
+  },[enteredEmail,enteredPassword])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+   
   };
 
   const passwordChangeHandler = (event) => {
